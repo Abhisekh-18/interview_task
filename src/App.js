4cc62f656c1f365bd22a028242609a007446e5cd
Abhisekh-18 <abhisekhsahoo5@gmail.com>
2021-05-31 17:06:14 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("#000000");
+  const changeColor = (e) => {
+    e.preventDefault();
+    let temp = "0123456789ABCDEF";
+    let initial = "#";
+    for (let i = 0; i < 6; i++) {
+      initial += temp[Math.floor(Math.random() * 16)];
+    }
+    setColor(initial);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        className="card"
+        onClick={(e) => changeColor(e)}
+        style={{
+          height: 500,
+          width: 500,
+          backgroundColor: color,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: 5,
+          boxShadow: "5px 5px 5px  #808080",
+        }}
+      >
+        <h1 style={{ textAlign: "center" }}>
+          This is some text within a card body.
+        </h1>
+      </div>
     </div>
   );
 }
